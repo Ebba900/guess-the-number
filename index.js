95bf48abfae1457f693189c0f7f7fb5de878a0b8
Ebba900 <ebba900@gmail.com>
2020-11-09 14:18:03 +0100

    3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Read instructions. And remove this line.
+
+const secretNumber = randomint ()
+function randomint () {
+    return (Math.floor () *101)
+} 
+
+function getUserGuess () { 
+    const stringvalue = document.getElementById("user-input").value
+    return parseInt(stringvalue, 10)
+
+}
+
+document.addEventListener("keyup") , function (event) {
+    if (event.ket === "enter") {
+        const guess = getguess ()
+        console.log(guess)
+    }
+}
